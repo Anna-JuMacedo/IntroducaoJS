972be1cc5e737569e72e37b7ef6d575c3281181e
Anna-JuMacedo <annajuliamacedo16@gmail.com>
2025-06-25 09:46:04 -0300

    calculadora js completa

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -4,3 +4,24 @@ function somar() {
     let soma = parseFloat(num1) + parseFloat(num2);
     document.getElementById("resultado").innerHTML = "Resultado: " + soma;
 }
+
+function subtracao() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let subtrair = parseFloat(num1) - parseFloat(num2);
+    document.getElementById("resultado").innerHTML = "Resultado: " + subtrair;
+}
+
+function multiplicacao() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let multiplicar = parseFloat(num1) * parseFloat(num2);
+    document.getElementById("resultado").innerHTML = "Resultado: " + multiplicar;
+}
+
+function divisao() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let dividir = parseFloat(num1) / parseFloat(num2);
+    document.getElementById("resultado").innerHTML = "Resultado: " + dividir;
+}
